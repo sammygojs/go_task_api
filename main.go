f74ff1970cb7837a2b207f6b186adf9a932a0790
Sammygojs <akoliyasumit@hotmail.com>
2025-05-05 23:52:14 +0100

    Tasks API using gin gonic

---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"net/http"
+)
+
+type Task struct {
+	ID     uint   `json:"id" gorm:"primaryKey"`
+	Title  string `json:"title"`
+	Status string `json:"status"`
+}
+
+var DB *gorm.DB
+
+func initDatabase() {
+	var err error
+	DB, err = gorm.Open(sqlite.Open("tasks.db"), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+	DB.AutoMigrate(&Task{})
+}
+
+func getTasks(c *gin.Context) {
+	var tasks []Task
+	DB.Find(&tasks)
+	c.JSON(http.StatusOK, tasks)
+}
+
+func getTask(c *gin.Context) {
+	id := c.Param("id")
+	var task Task
+	if err := DB.First(&task, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+		return
+	}
+	c.JSON(http.StatusOK, task)
+}
+
+func createTask(c *gin.Context) {
+	var task Task
+	if err := c.BindJSON(&task); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	DB.Create(&task)
+	c.JSON(http.StatusCreated, task)
+}
+
+func updateTask(c *gin.Context) {
+	id := c.Param("id")
+	var task Task
+	if err := DB.First(&task, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+		return
+	}
+	var input Task
+	if err := c.BindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	task.Title = input.Title
+	task.Status = input.Status
+	DB.Save(&task)
+	c.JSON(http.StatusOK, task)
+}
+
+func deleteTask(c *gin.Context) {
+	id := c.Param("id")
+	DB.Delete(&Task{}, id)
+	c.Status(http.StatusNoContent)
+}
+
+func main() {
+	initDatabase()
+
+	r := gin.Default()
+
+	r.GET("/tasks", getTasks)
+	r.GET("/tasks/:id", getTask)
+	r.POST("/tasks", createTask)
+	r.PUT("/tasks/:id", updateTask)
+	r.DELETE("/tasks/:id", deleteTask)
+
+	r.Run() // default :8080
+}
